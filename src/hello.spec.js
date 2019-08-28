1cbfd1d78cb429d6852f39164cfb060964267228
Gert Hengeveld <info@ghengeveld.nl>
2019-08-29 01:17:30 +0200

    Fix the issue.

--- src/hello.spec.js
+++ src/hello.spec.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { render } from "@testing-library/react"
+import { act } from "react-dom/test-utils"
+
+act(() => ({ then: () => {} })).then(() => {})
 
 const Hello = () => {
   const promise = Promise.reject("Oh no!")
